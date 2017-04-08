52006a01587c69bb61fb717f84324b3584dca774
Konstantin <iwebcat@yandex.ru>
2017-04-08 18:07:06 +1000

    Char
    problems solved

--- Assets/scripts/Base/CharacterBase.cs
+++ Assets/scripts/Base/CharacterBase.cs
@@ -10,30 +10,38 @@ public class CharacterBase : MonoBehaviour {
     public NetworkAdapter go;
     public int Hits;
     public int Str, Agi, Vit, Int, Wis, Chr;
-    public BoxCollider2D[] ObjectMass;
-    
+    public GameObject[] ObjectMass;
+    public BoxCollider2D[] InterMass;
 
-
+    public GameObject Inv;
     private void Start()
     {
+
+        ObjectMass = GameObject.FindGameObjectsWithTag("Obj");
         
-        ObjectMass = GameObject.Find("Object_obj").GetComponentsInChildren<BoxCollider2D>();
-       
+        InterMass = GameObject.Find("Interactive_obj").GetComponentsInChildren<BoxCollider2D>();
     }
 
     private void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.UpArrow)) MoveUp();
-        else if (Input.GetKeyDown(KeyCode.LeftArrow)) MoveLeft();
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) MoveDown();
-        else if (Input.GetKeyDown(KeyCode.RightArrow)) MoveRight();
+        if (!Inv.active)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) MoveUp();
+            else if (Input.GetKeyDown(KeyCode.LeftArrow)) MoveLeft();
+            else if (Input.GetKeyDown(KeyCode.DownArrow)) MoveDown();
+            else if (Input.GetKeyDown(KeyCode.RightArrow)) MoveRight();
+            if (gameObject.transform.position == new Vector3(14.0f, -2.0f, gameObject.transform.position.z))
+            {
+                gameObject.transform.position = new Vector3(63.0f, -118.0f);
+            }
+            else if (gameObject.transform.position == new Vector3(63.0f, -119.0f, gameObject.transform.position.z)) gameObject.transform.position = new Vector3(14.0f, -3.0f, gameObject.transform.position.z - 1.0f);
+        }
     }
     void MoveLeft()
     {
         GameObject.Find("troll").transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
         gameObject.transform.position += new Vector3(-1.0f,0);
-        foreach (BoxCollider2D pgo in ObjectMass)
+        foreach (GameObject pgo in ObjectMass)
         {
             
             if (gameObject.transform.position.x == pgo.transform.position.x && gameObject.transform.position.y == pgo.transform.position.y) gameObject.transform.position -= new Vector3(-1.0f, 0);
@@ -45,7 +53,7 @@ public class CharacterBase : MonoBehaviour {
     {
         GameObject.Find("troll").transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         gameObject.transform.position += new Vector3(1.0f, 0);
-        foreach (BoxCollider2D pgo in ObjectMass)
+        foreach (GameObject pgo in ObjectMass)
         {
             if (gameObject.transform.position.x == pgo.transform.position.x && gameObject.transform.position.y == pgo.transform.position.y) gameObject.transform.position -= new Vector3(1.0f, 0);
         }
@@ -54,7 +62,7 @@ public class CharacterBase : MonoBehaviour {
     void MoveUp()
     {
         gameObject.transform.position += new Vector3(0, 1.0f);
-        foreach (BoxCollider2D pgo in ObjectMass)
+        foreach (GameObject pgo in ObjectMass)
         {
             if (gameObject.transform.position.x == pgo.transform.position.x && gameObject.transform.position.y == pgo.transform.position.y) gameObject.transform.position -= new Vector3(0, 1.0f);
         }
@@ -63,7 +71,7 @@ public class CharacterBase : MonoBehaviour {
     void MoveDown()
     {
         gameObject.transform.position += new Vector3(0, -1.0f);
-        foreach (BoxCollider2D pgo in ObjectMass)
+        foreach (GameObject pgo in ObjectMass)
         {
             if (gameObject.transform.position.x == pgo.transform.position.x && gameObject.transform.position.y == pgo.transform.position.y) gameObject.transform.position -= new Vector3(0, -1.0f);
         }
